e32b4c5bf3b502959ad39317a82e3b1921d726dc
Olexyy <olexyy.mails@gmail.com>
2018-09-05 00:12:17 +0300

    Fix uninstall validator.

--- src/ContextsUninstallValidator.php
+++ src/ContextsUninstallValidator.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Drupal\contexts\UninstallValidator;
+namespace Drupal\contexts;
 
 use Drupal\contexts\Service\ContextsServiceInterface;
 use Drupal\Core\Extension\ModuleUninstallValidatorInterface;
@@ -41,24 +41,26 @@ class ContextsUninstallValidator implements ModuleUninstallValidatorInterface {
    */
   public function validate($module) {
 
-    $helperFieldService = $this->contextsService->getFieldHelper();
     $reasons = [];
-    foreach ($helperFieldService->getContentEntityTypeBundleInfo() as $entityTypeId => $bundleInfo) {
-      foreach ($bundleInfo as $entityBundle) {
-        if ($helperFieldService->getFieldConfig($entityTypeId, $entityBundle)) {
-          $reasons[] = $this->t('There is contexts field for the entity type: @entity_type, bundle: @bundle.', [
-            '@entity_type' => $entityTypeId,
-            '@bundle' => $entityBundle,
-          ]);
+    if ($module === 'contexts') {
+      $helperFieldService = $this->contextsService->getFieldHelper();
+      foreach ($helperFieldService->getContentEntityTypeBundleInfo() as $entityTypeId => $bundleInfo) {
+        foreach ($bundleInfo as $entityBundle => $bundleData) {
+          if ($helperFieldService->getFieldConfig($entityTypeId, $entityBundle)) {
+            $reasons[] = $this->t('There is contexts field for the entity type: @entity_type, bundle: @bundle', [
+              '@entity_type' => $entityTypeId,
+              '@bundle' => $entityBundle,
+            ]);
+          }
         }
       }
-    }
-    if ($reasons) {
-      $reasons[] = $this->t('Fields can be deleted in @contexts_dashboard.', [
-        '@contexts_dashboard' => Link::fromTextAndUrl($this->t('contexts dashboard'),
-          Url::fromRoute('contexts.dashboard')
-        ),
-      ]);
+      if ($reasons) {
+        $reasons[] = $this->t('Contexts fields can be deleted in @contexts_dashboard', [
+          '@contexts_dashboard' => Link::fromTextAndUrl($this->t('contexts dashboard'),
+            Url::fromRoute('contexts.dashboard')
+          )->toString(),
+        ]);
+      }
     }
 
     return $reasons;
